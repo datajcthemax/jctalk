8e738195f872731e312fae4ca3c8007f6b6a1c1d
datajcthemax <data.jcthemax@gmail.com>
2023-07-19 17:01:45 +0900

    server get, post

--- app/list/page.js
+++ app/list/page.js
@@ -1,25 +1,21 @@
 import { connectDB } from "@/util/database"
+import Link from "next/link"
+import { ObjectId } from "mongodb"
+
 
 export default async function List() {
-    const db = (await connectDB).db("jctalk")
-    let result = await db.collection('post').find().toArray()
-    console.log(result)
+  const db = (await connectDB).db("jctalk")
+  let result = await db.collection('post').find().toArray()
+  
 
-
-    return (
-      <div className="list-bg">
-        <div className="list-item">
-          <h4>{result[0].title}</h4>
-          <p>{result[0].content}</p>
+  return (
+    <div className="list-bg">
+      {result.map((post, index) => (
+        <div className="list-item" key={index}>
+          <Link href={`/detail/${result[index]._id}`}>{post.title}</Link>
+          <p>{post.date}</p>
         </div>
-        <div className="list-item">
-          <h4>{result[1].title}</h4>
-          <p>{result[1].content}</p>
-        </div>
-        <div className="list-item">
-          <h4>{result[2].title}</h4>
-          <p>{result[2].content}</p>
-        </div>
-      </div>
-    )
-  } 
+      ))}
+    </div>
+  )
+}
---
+++ app/write/page.js
@@ -0,0 +1,8 @@
+export default function Write() {
+    <div>
+        <h4>글작성</h4>
+        <form action="/api/test" method="POST">
+            <button type="submit">버튼</button>
+        </form>
+    </div>
+}

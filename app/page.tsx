3e48fff5c0dbd5801ce03a9f80fe7dd425bc8ad2
datajcthemax <data.jcthemax@gmail.com>
2023-07-19 14:38:17 +0900

    connected mongo db

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { MongoClient } from "mongodb"
+import { connectDB } from "@/util/database"
 
-
-export default function Home() {
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("jctalk")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+  
   return (
-   <div></div>
+   <div>hi</div>
   )
 }
